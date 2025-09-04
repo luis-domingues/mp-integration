1697392d05d7ef02d83e437f338278e59e2de462
luis-domingues <luisdomingues117@gmail.com>
2025-09-04 16:46:22 -0300

    feat(setup): add setup files with maven dependencies

---
+++ src/main/java/com/payment/mp_integration/MpIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.payment.mp_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpIntegrationApplication.class, args);
+	}
+
+}
