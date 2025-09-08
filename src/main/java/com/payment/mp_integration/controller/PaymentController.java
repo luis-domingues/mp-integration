ed68875c686e24010b7e7517dcdd41e7ea092e1a
luis-domingues <luisdomingues117@gmail.com>
2025-09-08 16:03:47 -0300

    faet(controller): add controllers to create payments + receive webhook

--- src/main/java/com/payment/mp_integration/controller/PaymentController.java
+++ src/main/java/com/payment/mp_integration/controller/PaymentController.java
@@ -1,4 +1,31 @@
 package com.payment.mp_integration.controller;
 
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.payment.Payment;
+import com.payment.mp_integration.dto.PaymentRequestDTO;
+import com.payment.mp_integration.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/payments")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody PaymentRequestDTO paymentRequestDTO) {
+        try {
+            Payment payment = paymentService.createPayment(paymentRequestDTO);
+            return ResponseEntity.ok(payment);
+        } catch(MPApiException | MPException exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", exception.getMessage()));
+        }
+    }
 }
--- src/main/java/com/payment/mp_integration/controller/WebhookController.java
+++ src/main/java/com/payment/mp_integration/controller/WebhookController.java
@@ -1,4 +1,18 @@
 package com.payment.mp_integration.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/webhook")
 public class WebhookController {
+    @PostMapping
+    public ResponseEntity<?> receiveWebhook(
+            @RequestBody Map<String, Object> payload,
+            @RequestHeader Map<String, String> headers) {
+        System.out.println("payload: " + payload);
+        return ResponseEntity.ok().build();
+    }
 }
