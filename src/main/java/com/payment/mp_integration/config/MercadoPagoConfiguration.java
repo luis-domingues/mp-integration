939b18515aef2cf92169cde695919396bb5ef26a
luis-domingues <luisdomingues117@gmail.com>
2025-09-08 15:53:36 -0300

    feat(config): implement secure credential configuration

--- src/main/java/com/payment/mp_integration/config/MercadoPagoConfiguration.java
+++ src/main/java/com/payment/mp_integration/config/MercadoPagoConfiguration.java
@@ -1,4 +1,12 @@
 package com.payment.mp_integration.config;
 
+import com.mercadopago.MercadoPagoConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MercadoPagoConfiguration {
+    public MercadoPagoConfiguration(@Value("${mp.access.token}") String accessToken) {
+        MercadoPagoConfig.setAccessToken(accessToken);
+    }
 }
